7ff527e84fcdbfb1945681957a28fff7c3c71d5f
shahab ali hassan <159798990+Shahab-Ali-E@users.noreply.github.com>
2025-12-23 14:06:19 +0500

    updated CV

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -159,7 +159,7 @@ export const HeroSection = () => {
 
           <button className="inline-flex items-center gap-2 bg-white h-12 px-7 rounded-xl border border-white">
             <a
-              href="/shahab_ali_ge_cv.pdf" 
+              href="/Shahab_Ali_CV.pdf" 
               download="shahab_ali_ge_cv"
               className="flex items-center gap-2"
             >
